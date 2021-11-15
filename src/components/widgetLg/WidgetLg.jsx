9513bc1142f0c15484f248f2b1419421d21bd090
Nath <yankahnathanieltopp@gmail.com>
2021-11-15 14:05:08 +0000

    home page completed

---
+++ src/components/widgetLg/WidgetLg.jsx
@@ -0,0 +1,53 @@
+import "./widgetlg.css";
+
+export default function WidgetLg() {
+    const Button = ({type}) => {
+        return <button className={`widgetLgButton ${type}`}>{type}</button>
+    }
+    return (
+        <div className="widgetLg">
+            <h3 className="widgetLgTitle">Transactions</h3>
+            <table className="widgetLgTable">
+                <tr className="widgetLgTr">
+                    <th className="wigetLgTh">Customer</th>
+                    <th className="wigetLgTh">Date</th>
+                    <th className="wigetLgTh">Amount</th>
+                    <th className="wigetLgTh">Status</th>
+                </tr>
+                <tr className="widgetLgTr">
+                    <td className="widgetLgUser">
+                        <img className="widgetLgImage" src="images/user1.jpeg" alt="user1" />
+                        <span className="widgetLgName">Nathaniel Topp</span>
+                    </td>
+                    <td className="widgetLgDate">2 Jun 2021</td>
+                    <td className="widgetLgAmount">$122.00</td>
+                    <td className="widgetLgStatus">
+                        <Button type="Approved"></Button>
+                    </td>
+                </tr>
+                <tr className="widgetLgTr">
+                    <td className="widgetLgUser">
+                        <img className="widgetLgImage" src="images/user1.jpeg" alt="user1" />
+                        <span className="widgetLgName">Nathaniel Topp</span>
+                    </td>
+                    <td className="widgetLgDate">2 Jun 2021</td>
+                    <td className="widgetLgAmount">$122.00</td>
+                    <td className="widgetLgStatus">
+                        <Button type="Declined"></Button>
+                    </td>
+                </tr>
+                <tr className="widgetLgTr">
+                    <td className="widgetLgUser">
+                        <img className="widgetLgImage" src="images/user1.jpeg" alt="user1" />
+                        <span className="widgetLgName">Nathaniel Topp</span>
+                    </td>
+                    <td className="widgetLgDate">2 Jun 2021</td>
+                    <td className="widgetLgAmount">$122.00</td>
+                    <td className="widgetLgStatus">
+                        <Button type="Pending"></Button>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    )
+}
---
+++ src/components/widgetSm/WidgetSm.jsx
@@ -0,0 +1,68 @@
+import { Visibility } from "@material-ui/icons";
+import "./widgetsm.css";
+
+export default function WidgetSm() {
+    return (
+        <div className="widgetSm">
+            <span className="widgetSmTitle">New Join Members</span>
+            <ul className="widgetSmList">
+                <li className="widgetSmListItem">
+                    <img className="widgetSmImage" src="images/user1.jpeg" alt="user image 1" />
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUsername">Anna Keller</span>
+                        <span className="widgetSmUserTitle">Software Engineer</span>
+                    </div>
+                    <button className="widgetSmButton">
+                        <Visibility className="widgetSmIcon" />
+                        Display
+                    </button>
+                </li>
+                <li className="widgetSmListItem">
+                    <img className="widgetSmImage" src="images/user1.jpeg" alt="user image 1" />
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUsername">Anna Keller</span>
+                        <span className="widgetSmUserTitle">Software Engineer</span>
+                    </div>
+                    <button className="widgetSmButton">
+                        <Visibility className="widgetSmIcon" />
+                        Display
+                    </button>
+                </li>
+                <li className="widgetSmListItem">
+                    <img className="widgetSmImage" src="images/user1.jpeg" alt="user image 1" />
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUsername">Anna Keller</span>
+                        <span className="widgetSmUserTitle">Software Engineer</span>
+                    </div>
+                    <button className="widgetSmButton">
+                        <Visibility className="widgetSmIcon" />
+                        Display
+                    </button>
+                </li>
+                <li className="widgetSmListItem">
+                    <img className="widgetSmImage" src="images/user1.jpeg" alt="user image 1" />
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUsername">Anna Keller</span>
+                        <span className="widgetSmUserTitle">Software Engineer</span>
+                    </div>
+                    <button className="widgetSmButton">
+                        <Visibility className="widgetSmIcon" />
+                        Display
+                    </button>
+                </li>
+                <li className="widgetSmListItem">
+                    <img className="widgetSmImage" src="images/user1.jpeg" alt="user image 1" />
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUsername">Anna Keller</span>
+                        <span className="widgetSmUserTitle">Software Engineer</span>
+                    </div>
+                    <button className="widgetSmButton">
+                        <Visibility className="widgetSmIcon" />
+                        Display
+                    </button>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
