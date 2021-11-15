3e3d917df01bccec56d4599510798ea935d01912
Nath <yankahnathanieltopp@gmail.com>
2021-11-15 12:05:39 +0000

    chart component complete

---
+++ src/dummyData.js
@@ -0,0 +1,50 @@
+export const userData = [
+    {
+      name: 'Jan',
+      activeUser: 4000
+    },
+    {
+        name: 'Feb',
+        activeUser: 3000
+    },
+    {
+        name: 'Mar',
+        activeUser: 4000
+    },
+    {
+        name: 'Apr',
+        activeUser: 5000
+    },
+    {
+        name: 'May',
+        activeUser: 2000
+    },
+    {
+        name: 'Jun',
+        activeUser: 3000
+        },
+    {
+        name: 'Jul',
+        activeUser: 4000
+    },
+    {
+        name: 'Aug',
+        activeUser: 4000
+    },
+    {
+        name: 'Sep',
+        activeUser: 3000
+    },
+    {
+        name: 'Oct',
+        activeUser: 2000
+    },
+    {
+        name: 'Nov',
+        activeUser: 1000
+    },
+    {
+        name: 'Dec',
+        activeUser: 4000
+    }
+  ];
